031cce78e0dfa93571db944d87a1c9dbc8c2ae23
Nicolas Beaudouin <n.beaudouin@outlook.fr>
2017-03-01 17:04:46 +0100

    Add SharedPreferences system

--- app/src/main/java/nicolas/beaudouin/mytodolist/AddTodoActivity.java
+++ app/src/main/java/nicolas/beaudouin/mytodolist/AddTodoActivity.java
@@ -1,10 +1,14 @@
 package nicolas.beaudouin.mytodolist;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by nico on 01/03/17.
@@ -13,6 +17,7 @@ import android.widget.EditText;
 public class AddTodoActivity extends Activity implements View.OnClickListener {
 
     private StockageValeurs newToDo = StockageValeurs.getInstance();
+    public static final String PREFS_NAME = "MyDataContainer";
 
     private EditText todoText;
     private Button btnAddTodo;
@@ -34,6 +39,18 @@ public class AddTodoActivity extends Activity implements View.OnClickListener {
 
         switch(v.getId()) {
             case R.id.btnAddTodo:
+                // utilisation d'un SharedPreferences editor
+                SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+                SharedPreferences.Editor editor = settings.edit(); // création d'un editor pour
+                                                                    // l'ajout/suppresion/modification
+                                                                    // du shared preferences
+                Set<String> todoItemValue = new HashSet<String>();
+                todoItemValue.add(todoText.getText().toString());
+                editor.putStringSet("todoItem", todoItemValue);
+
+                //commit des données à insérer
+                editor.commit();
+
                 newToDo.setValues(todoText.getText().toString());
                 todoText.setText("");
                 setResult(RESULT_OK);
--- app/src/main/java/nicolas/beaudouin/mytodolist/TodoActivity.java
+++ app/src/main/java/nicolas/beaudouin/mytodolist/TodoActivity.java
@@ -2,14 +2,14 @@ package nicolas.beaudouin.mytodolist;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by nico on 01/03/17.
@@ -23,6 +23,8 @@ public class TodoActivity extends Activity implements View.OnClickListener {
     static final int ADD_TODO_ITEM = 1;
 
     private StockageValeurs values = StockageValeurs.getInstance();
+    public static final String PREFS_NAME = "MyDataContainer";
+    private ArrayList<String> todoItems = new ArrayList<String>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,10 +42,14 @@ public class TodoActivity extends Activity implements View.OnClickListener {
         addTodo.setOnClickListener(this);
         dropList.setOnClickListener(this);
 
+        // récupération des informations du SharedPreferences
+        SharedPreferences setting = getSharedPreferences(PREFS_NAME, 0);
+        todoItems.add(setting.getString("todoItem", ""));
+
         // insertion des données a l'intérieur de la list view
         historicText.setAdapter(new ArrayAdapter<String>(getApplicationContext(),
                 android.R.layout.simple_list_item_1,
-                values.getValues()));
+                /*values.getValues()*/ todoItems));
 
     }
 
